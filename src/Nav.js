7e2001e6a2097a448104cb57986af9d17c021526
Sam Carpenter-Camhi <126645548+sama-cc@users.noreply.github.com>
2023-06-21 07:18:46 +0000

    Update Nav.js Created Buttons use Material UI

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
 
 function Nav() {
-  return "This is the Nav.";
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          "& > *": {
+            m: 1,
+          },
+        }}
+      >
+        <ButtonGroup variant="text" aria-label="text button group">
+          <Button>Characters</Button>
+          <Button>Teams</Button>
+        </ButtonGroup>
+      </Box>
+    </div>
+  );
 }
 
 export default Nav;
