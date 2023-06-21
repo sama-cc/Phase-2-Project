b00a50f505daf625309f0188e06e864a4e8c9096
Sam Carpenter-Camhi <126645548+sama-cc@users.noreply.github.com>
2023-06-21 07:50:34 +0000

    Update TeamForm.js Added team name form input

--- src/TeamForm.js
+++ src/TeamForm.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function TeamForm() {
-  return "This is the TeamForm.";
+  return (
+    <div>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            id="team-name"
+            label="Team Name"
+            defaultValue=""
+            size="small"
+          />
+        </div>
+      </Box>
+    </div>
+  );
 }
 
 export default TeamForm;
