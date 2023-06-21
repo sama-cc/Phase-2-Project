cd04f534c72f887a1817db84634367bd6116c6a6
Sam Carpenter-Camhi <126645548+sama-cc@users.noreply.github.com>
2023-06-21 06:59:17 +0000

    Update Header.js Added login button

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
 
 function Header() {
+  const [login, setLogin] = useState(false);
+
+  function handleLogin() {
+    setLogin(!login);
+  }
+
   return (
     <div className="main-header">
-      "This is the Header."
+      <Button variant="contained" onClick={handleLogin}>
+        {login ? "Logout" : "Login"}
+      </Button>
     </div>
   );
 }
