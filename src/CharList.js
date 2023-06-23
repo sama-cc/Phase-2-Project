17462391b8ee6f87c0bfe6f279b325c57d6413d7
Sam Carpenter-Camhi <126645548+sama-cc@users.noreply.github.com>
2023-06-23 04:39:13 +0000

    Update CharList.js and index.css fixed issues with card images not showing up for some characters and also the scale of th images

--- src/CharList.js
+++ src/CharList.js
@@ -6,6 +6,51 @@ import Grid from "@mui/material/Grid";
 import StarIcon from "@mui/icons-material/Star";
 
 function CharList({ characters }) {
+  function handleName(name) {
+    switch (name) {
+      case "hu tao":
+        return "hu-tao";
+      case "kazuha":
+        return "kaedehara-kazuha";
+      case "ayaka":
+        return "kamisato-ayaka";
+      case "ayato":
+        return "kamisato-ayato";
+      case "sara":
+        return "kujou-sara";
+      case "raiden shogun":
+        return "raiden-shogun";
+      case "kokomi":
+        return "sangonomiya-kokomi";
+      case "itto":
+        return "arataki-itto";
+      case "yun jin":
+        return "yun-jin";
+      case "yae":
+        return "yae-miko";
+      case "kuki":
+        return "kuki-shinobu";
+      default:
+        return name;
+    }
+  }
+
+  function handleAetherLumine(name) {
+    return name === "Traveller (male)" ? (
+      <img
+        src={`https://static.wikia.nocookie.net/gensin-impact/images/a/a5/Aether_Icon.png`}
+        alt={name}
+        className="card"
+      />
+    ) : (
+      <img
+        src={`https://static.wikia.nocookie.net/gensin-impact/images/9/9c/Lumine_Icon.png`}
+        alt={name}
+        className="card"
+      />
+    );
+  }
+
   const Item = styled(Paper)(() => ({
     backgroundColor: "lightskyblue",
     padding: 8,
@@ -14,12 +59,21 @@ function CharList({ characters }) {
   }));
   const charList = characters.map((char) => {
     return (
-      <Grid key={char} item xs={3}>
+      <Grid key={char.id} item xs={3}>
         <Item elevation={3}>
           <h3>{char.name}</h3>
-          <img
-            src={`https://cdn.wanderer.moe/genshin-impact/character-icons/${char.name.toLowerCase()}-icon.png`}
-          />
+          {char.name === "Traveller (male)" ||
+          char.name === "Traveller (female)" ? (
+            handleAetherLumine(char.name)
+          ) : (
+            <img
+              src={`https://cdn.wanderer.moe/genshin-impact/character-icons/${handleName(
+                char.name.toLowerCase()
+              )}-icon.png`}
+              alt={char.name}
+              className="card"
+            />
+          )}
           <p>{char.vision}</p>
           <p>{char.weapon}</p>
           <p>
