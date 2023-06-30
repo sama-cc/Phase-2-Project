c225ee315c4a04a0c76cd9601170847d1dc5c072
Sam Carpenter-Camhi <126645548+sama-cc@users.noreply.github.com>
2023-06-30 03:29:44 +0000

    Update SearchBar.js added the ability to filter characters by a combination of vision, weapon and rarity

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
 
 export default function SearchBar({
   Item,
@@ -10,14 +11,115 @@ export default function SearchBar({
   characters,
 }) {
   const [searchText, setSearchText] = useState("");
+  const [visionSelect, setVisionSelect] = useState("");
+  const [weaponSelect, setWeaponSelect] = useState("");
+  const [raritySelect, setRaritySelect] = useState("");
 
   function handleSearch() {
     setCharsToDisplay(
-      charsToDisplay.filter(
+      characters.filter(
         (char) => char.name.toLowerCase() === searchText.toLowerCase()
       )
     );
   }
+
+  function handleFilter() {
+    if (visionSelect !== "" && weaponSelect === "" && raritySelect === "") {
+      return setCharsToDisplay(
+        characters.filter(
+          (char) => char.vision.toLowerCase() === visionSelect.toLowerCase()
+        )
+      );
+    } else if (
+      visionSelect === "" &&
+      weaponSelect !== "" &&
+      raritySelect === ""
+    ) {
+      return setCharsToDisplay(
+        characters.filter(
+          (char) => char.weapon.toLowerCase() === weaponSelect.toLowerCase()
+        )
+      );
+    } else if (
+      visionSelect === "" &&
+      weaponSelect === "" &&
+      raritySelect !== ""
+    ) {
+      return setCharsToDisplay(
+        characters.filter(
+          (char) => char.rarity.toLowerCase() === raritySelect.toLowerCase()
+        )
+      );
+    } else if (
+      visionSelect !== "" &&
+      weaponSelect !== "" &&
+      raritySelect === ""
+    ) {
+      setCharsToDisplay(
+        characters
+          .filter(
+            (char) => char.vision.toLowerCase() === visionSelect.toLowerCase()
+          )
+          .filter(
+            (char) => char.weapon.toLowerCase() === weaponSelect.toLowerCase()
+          )
+      );
+    } else if (
+      visionSelect !== "" &&
+      weaponSelect === "" &&
+      raritySelect !== ""
+    ) {
+      setCharsToDisplay(
+        characters
+          .filter(
+            (char) => char.vision.toLowerCase() === visionSelect.toLowerCase()
+          )
+          .filter(
+            (char) => char.rarity.toLowerCase() === raritySelect.toLowerCase()
+          )
+      );
+    } else if (
+      visionSelect === "" &&
+      weaponSelect !== "" &&
+      raritySelect !== ""
+    ) {
+      setCharsToDisplay(
+        characters
+          .filter(
+            (char) => char.weapon.toLowerCase() === weaponSelect.toLowerCase()
+          )
+          .filter(
+            (char) => char.rarity.toLowerCase() === raritySelect.toLowerCase()
+          )
+      );
+    } else if (
+      visionSelect !== "" &&
+      weaponSelect !== "" &&
+      raritySelect !== ""
+    ) {
+      setCharsToDisplay(
+        characters
+          .filter(
+            (char) => char.vision.toLowerCase() === visionSelect.toLowerCase()
+          )
+          .filter(
+            (char) => char.weapon.toLowerCase() === weaponSelect.toLowerCase()
+          )
+          .filter(
+            (char) => char.rarity.toLowerCase() === raritySelect.toLowerCase()
+          )
+      );
+    }
+  }
+
+  function handleReset() {
+    setSearchText("");
+    setVisionSelect("");
+    setWeaponSelect("");
+    setRaritySelect("");
+    setCharsToDisplay(characters);
+  }
+
   return (
     <Grid sx={{ m: 4 }} item xs={12}>
       <Item elevation={8}>
@@ -38,10 +140,65 @@ export default function SearchBar({
         >
           Search
         </Button>
+        <TextField
+          id="vision-select"
+          select
+          label="Vision"
+          size="small"
+          name="vision-select"
+          onChange={(e) => setVisionSelect(e.target.value)}
+          value={visionSelect}
+          style={{ width: 100 }}
+        >
+          <MenuItem value="Anemo">Anemo</MenuItem>
+          <MenuItem value="Cryo">Cryo</MenuItem>
+          <MenuItem value="Dendro">Dendro</MenuItem>
+          <MenuItem value="Electro">Electro</MenuItem>
+          <MenuItem value="Geo">Geo</MenuItem>
+          <MenuItem value="Hydro">Hydro</MenuItem>
+          <MenuItem value="Pyro">Pyro</MenuItem>
+        </TextField>
+        <TextField
+          id="weapon-select"
+          select
+          label="Weapon"
+          size="small"
+          name="weapon-select"
+          onChange={(e) => setWeaponSelect(e.target.value)}
+          value={weaponSelect}
+          style={{ width: 100 }}
+        >
+          <MenuItem value="Bow">Bow</MenuItem>
+          <MenuItem value="Catalyst">Catalyst</MenuItem>
+          <MenuItem value="Claymore">Claymore</MenuItem>
+          <MenuItem value="Polearm">Polearm</MenuItem>
+          <MenuItem value="Sword">Sword</MenuItem>
+        </TextField>
+        <TextField
+          id="rarity-select"
+          select
+          label="Rarity"
+          size="small"
+          name="rarity-select"
+          onChange={(e) => setRaritySelect(e.target.value)}
+          value={raritySelect}
+          style={{ width: 100 }}
+        >
+          <MenuItem value="4_Star">4 Star</MenuItem>
+          <MenuItem value="5_Star">5 Star</MenuItem>
+        </TextField>
+        <Button
+          sx={{ m: 1 }}
+          variant="contained"
+          onClick={handleFilter}
+          size="small"
+        >
+          Filter
+        </Button>
         <Button
           sx={{ m: 0 }}
           variant="contained"
-          onClick={() => setCharsToDisplay(characters)}
+          onClick={handleReset}
           size="small"
         >
           Reset
